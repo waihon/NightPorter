a9f647f4c85de9f71a5949c9be5790b0444b1523
Waihon Yew <yewwaihon@gmail.com>
2020-03-22 22:23:03 +0800

    Create string array of tasks

--- NightPorter/ViewController.swift
+++ NightPorter/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Create string array of tasks
+    let dailyTasks = ["Check all windows",
+                      "Check all doors",
+                      "Is the boiler fueled?",
+                      "Check the mailbox",
+                      "Empty trash containers",
+                      "If freezing, check water pipes",
+                      "Document \"strange & unusual\" occurrences"]
+
+    let weeklyTasks = ["Check inside all cabins",
+                       "Flush all lavatories in cabins",
+                       "Walk the perimeter of property"]
+
+    let monthlyTasks = ["Test security alarm",
+                        "Test motion detectors",
+                        "Test smoke alarms"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
