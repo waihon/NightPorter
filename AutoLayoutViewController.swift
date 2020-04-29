e7ae418f3afadeeebffcf3b5535079efdf78e78b
Waihon Yew <yewwaihon@gmail.com>
2020-04-29 23:51:29 +0800

    Add the corresponding class file for Auto Layout scene

---
+++ AutoLayoutViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AutoLayoutViewController.swift
+//  NightPorter
+//
+//  Created by Waihon Yew on 4/29/20.
+//  Copyright © 2020 Waihon Yew. All rights reserved.
+//
+
+import UIKit
+
+class AutoLayoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
