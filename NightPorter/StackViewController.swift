b396cdc264114ef21803b80e4b6f70d647f1e405
Waihon Yew <yewwaihon@gmail.com>
2020-05-02 00:41:25 +0800

    Create and connect Stack View controller and custom class; customize the corresponding tab bar item

---
+++ NightPorter/StackViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StackViewController.swift
+//  NightPorter
+//
+//  Created by Waihon Yew on 5/2/20.
+//  Copyright © 2020 Waihon Yew. All rights reserved.
+//
+
+import UIKit
+
+class StackViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
