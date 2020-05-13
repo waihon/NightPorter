946e801dca81fe3ebe96eb266d0f2fd52d60deec
Waihon Yew <yewwaihon@gmail.com>
2020-05-13 23:49:07 +0800

    Add a new Phone scene, a new view controller class, and connect both of them

---
+++ NightPorter/PhoneViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PhoneViewController.swift
+//  NightPorter
+//
+//  Created by Waihon Yew on 5/13/20.
+//  Copyright © 2020 Waihon Yew. All rights reserved.
+//
+
+import UIKit
+
+class PhoneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
