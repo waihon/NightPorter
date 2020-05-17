4c30a09db5ff6f89d65fce48496448dc53eb6c33
Waihon Yew <yewwaihon@gmail.com>
2020-05-17 13:38:03 +0800

    Create a new Contact Us scene and view controller and connect them

---
+++ NightPorter/ContactUsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactUsViewController.swift
+//  NightPorter
+//
+//  Created by Waihon Yew on 5/17/20.
+//  Copyright © 2020 Waihon Yew. All rights reserved.
+//
+
+import UIKit
+
+class ContactUsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
