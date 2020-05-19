13dce490f057ca5725b183e1bfd7ba768c947757
Waihon Yew <yewwaihon@gmail.com>
2020-05-20 00:04:51 +0800

    Connect Scroll View to the corresponding view controller as a Weak Outlet

--- NightPorter/ContactUsViewController.swift
+++ NightPorter/ContactUsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ContactUsViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
