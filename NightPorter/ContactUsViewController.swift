126aa26af4565dd0e43afcf5fb354db9874f7545
Waihon Yew <yewwaihon@gmail.com>
2020-05-24 11:58:44 +0800

    Manually add the Scroll to the controll'rs main view

--- NightPorter/ContactUsViewController.swift
+++ NightPorter/ContactUsViewController.swift
@@ -16,6 +16,7 @@ class ContactUsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.addSubview(scrollView)
     }
     
 
